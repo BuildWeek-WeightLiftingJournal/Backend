4e743613accbb0abfa2052c0be2ea540df89e699
Adan <51211285+Adan011@users.noreply.github.com>
2019-10-23 23:28:00 -0500

    added some Swagger

--- journal/src/main/java/com/lambdaschool/journal/controller/WorkoutController.java
+++ journal/src/main/java/com/lambdaschool/journal/controller/WorkoutController.java
@@ -1,10 +1,13 @@
 package com.lambdaschool.journal.controller;
 
+import com.lambdaschool.journal.models.ErrorDetail;
 import com.lambdaschool.journal.models.User;
 import com.lambdaschool.journal.models.Workout;
 import com.lambdaschool.journal.service.UserService;
 import com.lambdaschool.journal.service.WorkoutService;
 import io.swagger.annotations.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
@@ -15,6 +18,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -28,11 +32,10 @@ public class WorkoutController
     @Autowired
     private UserService userService;
 
-    @ApiOperation(value = "lists all workouts",
-                  response = Workout.class,
-                  responseContainer = "List")
+    private static final Logger logger = LoggerFactory.getLogger(WorkoutController.class);
 
     //GET http://localhost:2019/workout/all
+    @ApiOperation(value = "lists all workouts", response = Workout.class, responseContainer = "List")
     @GetMapping(value = "/workout/all",
                 produces = {"application/json"})
     public ResponseEntity<?> listAllWorkouts()
@@ -56,11 +59,15 @@ public class WorkoutController
 //    }
 
     //POST http://localhost:2019/workout/new
-    @ApiOperation(value = "adds a new workout")
+    @ApiOperation(value = "adds a new workout to logged in user only")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Workout added to User"),
+                          })
     @PostMapping(value = "/workout/new", consumes = {"application/json"}, produces = {"application/json"})
     public ResponseEntity<?> addWorkout(@Valid
-                                           @RequestBody Workout workout) throws URISyntaxException
+                                           @RequestBody Workout workout, HttpServletRequest request) throws URISyntaxException
     {
+        logger.info(request.getMethod() + " " + request.getRequestURI() + " added");
+
         workoutService.save(workout);
         HttpHeaders responseHeaders = new HttpHeaders();
         URI newWorkoutURI = ServletUriComponentsBuilder
@@ -68,24 +75,35 @@ public class WorkoutController
                 .buildAndExpand(workout.getWorkoutid())
                 .toUri();
         responseHeaders.setLocation(newWorkoutURI);
-        return new ResponseEntity<>(HttpStatus.CREATED);
+        return new ResponseEntity<>(null, responseHeaders, HttpStatus.CREATED);
     }
 
     //PUT http://localhost:2019/workout/update/16
+    @ApiOperation(value = "updates a workout")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Workout updated ", response = Workout.class),
+                          @ApiResponse(code = 404, message = "Workout not found", response = ErrorDetail.class)})
     @PutMapping(value = "workout/update/{id}",
                         consumes = {"application/json"})
     public ResponseEntity<?> updateWorkout(@Valid
-                                           @RequestBody Workout workout,
-                                           @PathVariable long id)
+                                           @RequestBody Workout updateWorkout,
+                                           @ApiParam(value = "Workout id",
+                                                     required = true,
+                                                     example = "1")
+                                           @PathVariable long id, HttpServletRequest request)
     {
-        workoutService.update(workout,id);
+        logger.info(request.getMethod() + " " + request.getRequestURI() + " updated " + id);
+        workoutService.update(updateWorkout,id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     //DELETE http://localhost:2019/workout/delete/15
+    @ApiOperation(value = "deletes a workout")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Workout deleted", response = Workout.class),
+                            @ApiResponse(code = 400, message = "Workout not found", response = ErrorDetail.class)})
     @DeleteMapping(value = "/workout/delete/{id}")
-    public ResponseEntity<?> deleteWorkout(@PathVariable long id)
+    public ResponseEntity<?> deleteWorkout(@PathVariable long id, HttpServletRequest request)
     {
+        logger.info(request.getMethod() + " " + request.getRequestURI() + " deleted " + id);
         workoutService.delete(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
